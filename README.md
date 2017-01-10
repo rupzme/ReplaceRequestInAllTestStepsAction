863d80084870edae8dfba09399e726a63a835738
Rupert Anderson <bearsoftware@yahoo.co.uk>
2017-01-10 11:15:54 +0000

    [ra] more tidying

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ For usage and documentation please see the related blog article at:
 
 1. To build: `./gradlew clean build` or `gradle clean build` (if you have Gradle installed)
 2. Copy `<project home>/build/libs/ReplaceRequestInAllTestStepsAction-1.0-lib.jar` to `<SoapUI Home>/java/app/bin/ext/`
-2. Copy `<project home>/src/main/resources/custom-actions.xml` to `<SoapUI Home>/java/app/bin/actions/`
+3. Copy `<project home>/src/main/resources/custom-actions.xml` to `<SoapUI Home>/java/app/bin/actions/`
+4. Start / restart SoapUI
 
 ###Tasks Remaining
 - [x] Have done basic manual testing in SoapUI
+- [ ] Release to SoapUI community and get feedback
--- src/test/groovy/bsl/custom/actions/ReplaceRequestInAllTestStepsActionSpec.groovy
+++ src/test/groovy/bsl/custom/actions/ReplaceRequestInAllTestStepsActionSpec.groovy
@@ -2,30 +2,22 @@ package bsl.custom.actions
 
 import bsl.custom.TestStepSelector
 import bsl.custom.TestStepUpdater
-import bsl.custom.actions.ReplaceRequestInAllTestStepsAction
 import com.eviware.soapui.impl.rest.RestRequest
-import com.eviware.soapui.impl.rest.RestRequestInterface
 
 /**
  * Created by Rupert on 02/01/2017.
  */
-
-//TODO Improve test to stub testStepSelector.. to return a List<TestStep>
-//TODO Improve test to stub restRequest.requestContent to return an int
-//TODO and add this as a parameter when checking invocation of testStepUpdater.replaceContentInRelatedTestSteps
 class ReplaceRequestInAllTestStepsActionSpec extends spock.lang.Specification {
 
-    ReplaceRequestInAllTestStepsAction replaceRequestInAllTestStepsAction = new ReplaceRequestInAllTestStepsAction()
-    TestStepSelector testStepSelector = Mock()
-    TestStepUpdater testStepUpdater = Mock()
-    RestRequest restRequest = Mock()
-
-    def setup(){
-        replaceRequestInAllTestStepsAction.testStepsSelector = testStepSelector
-        replaceRequestInAllTestStepsAction.testStepsUpdater = testStepUpdater
-    }
-
     def "Should call Selector and Updater dependencies once when perform method is called"() {
+        given:
+            ReplaceRequestInAllTestStepsAction replaceRequestInAllTestStepsAction = new ReplaceRequestInAllTestStepsAction()
+            TestStepSelector testStepSelector = Mock()
+            TestStepUpdater testStepUpdater = Mock()
+            RestRequest restRequest = Mock()
+            replaceRequestInAllTestStepsAction.testStepsSelector = testStepSelector
+            replaceRequestInAllTestStepsAction.testStepsUpdater = testStepUpdater
+
         when:
             replaceRequestInAllTestStepsAction.perform(restRequest, null)
 
